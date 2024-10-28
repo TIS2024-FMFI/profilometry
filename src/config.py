4b5899a29edec22abef0d1f794169d204373ec1f
lukashajduch <160210975+LukasHajduch4@users.noreply.github.com>
2024-10-28 12:49:30 +0100

    Prvý commit, main-window a zobrazovanie okna s cam

---
+++ src/config.py
@@ -0,0 +1,14 @@
+# Konfigurácia okna
+WINDOW_CONFIG = {
+    'title': 'LaserScan Pro',
+    'width_ratio': 0.8,
+    'height_ratio': 0.8,
+    'bg_color': '#F0F8FF'
+}
+
+# Konfigurácia tlačidiel
+BUTTON_CONFIG = {
+    'width': 20,
+    'padding': 10,
+    'font': ('Arial', 11)
+}
